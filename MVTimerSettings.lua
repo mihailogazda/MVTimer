2a9332d8d5a2109b1d7d2b9c597eab3fa2637c68
mihailogazda <mihailogazda@gmail.com>
2014-09-09 21:38:36 +0200

    Code review and cleanup

--- MVTimerSettings.lua
+++ MVTimerSettings.lua
@@ -4,17 +4,18 @@ MVTimerSettings = {}
 
 -- Key 1 settings
 
-MVTimerSettings[1] = {}
-MVTimerSettings[1].text = "This is buff 1"
-MVTimerSettings[1].duration = 30
+MVTimerSettings[KEY_BUFF_1] = {}
+MVTimerSettings[KEY_BUFF_1].text = "This is buff 1"
+MVTimerSettings[KEY_BUFF_1].duration = 30
 
 -- Key 2 settings
-MVTimerSettings[2] = {}
-MVTimerSettings[2].text = "This is buff 2"
-MVTimerSettings[2].duration = 20
+MVTimerSettings[KEY_BUFF_2] = {}
+MVTimerSettings[KEY_BUFF_2].text = "This is buff 2"
+MVTimerSettings[KEY_BUFF_2].duration = 20
 
 -- Key 3 settings
-MVTimerSettings[3] = {}
-MVTimerSettings[3].text = "This is buff 3"
-MVTimerSettings[3].duration = 10
+MVTimerSettings[KEY_BUFF_3] = {}
+MVTimerSettings[KEY_BUFF_3].text = "This is buff 3"
+MVTimerSettings[KEY_BUFF_3].duration = 10
+
 
